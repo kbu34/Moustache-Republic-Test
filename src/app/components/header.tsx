213faa104e8c8953e4e41c36be077ce66258fac7
Phillip Kim <phillipkim156@gmail.com>
2024-04-21 14:12:42 +1200

    Set up header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="bg-header-bg mt-5 flex items-center justify-end px-20 py-2">
+      <button className="text-font-light ml-10 text-xs hover:border-b">
+        My Cart(1)
+      </button>
+    </div>
+  );
+}
